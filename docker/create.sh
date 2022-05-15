c4868a22a1ae5bead86e2b538f165dcc2ede3d64
jkarolczak <jacek.karolczak@outlook.com>
2022-05-15 17:15:00 +0200

    Rename cluster

--- docker/create.sh
+++ docker/create.sh
@@ -1,4 +1,4 @@
-docker run --name cas1 -p 9042:9042 -e CASSANDRA_CLUSTER_NAME=MyCluster -d cassandra
-docker run --name cas2 -e CASSANDRA_SEEDS="$(docker inspect --format='{{.NetworkSettings.IPAddress}}' cas1)" -e CASSANDRA_CLUSTER_NAME=MyCluster -d cassandra
-# docker run --name cas3 -e CASSANDRA_SEEDS="$(docker inspect --format='{{.NetworkSettings.IPAddress}}' cas1)" -e CASSANDRA_CLUSTER_NAME=MyCluster -d cassandra
+docker run --name cas1 -p 9042:9042 -e CASSANDRA_CLUSTER_NAME=BigTrains -d cassandra
+docker run --name cas2 -e CASSANDRA_SEEDS="$(docker inspect --format='{{.NetworkSettings.IPAddress}}' cas1)" -e CASSANDRA_CLUSTER_NAME=BigTrains -d cassandra
+# docker run --name cas3 -e CASSANDRA_SEEDS="$(docker inspect --format='{{.NetworkSettings.IPAddress}}' cas1)" -e CASSANDRA_CLUSTER_NAME=BigTrains -d cassandra
 docker exec -ti cas1 nodetool -Dcom.sun.jndi.rmiURLParsing=legacy status
