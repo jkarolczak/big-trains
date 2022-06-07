4a0f490c2e8e065d47b2f3908e6f6e84c46be853
jkarolczak <jacek.karolczak@outlook.com>
2022-06-07 21:07:01 +0200

    Add stress tests 1 and 3

--- app.py
+++ app.py
@@ -1,60 +1,34 @@
-import pandas as pd
-import streamlit as st
-
-from src import CassandraConnector, Config, utils
-
-
-def admin() -> None:
-    st.title("Admin panel")
-    col1, col2 = st.columns(2)
-    with col1:
-        st.subheader("Fill database")
-        if st.button("Create 20 artificial runs"):
-            utils.fill_database(st.session_state["client"], 20)
-    with col2:
-        st.subheader("Truncate all tables")
-        if st.button("Truncate all tables"):
-            st.session_state["client"].query_engine.truncate.all()
-
-
-def reservations() -> None:
-    st.title("Reservations")
-
-
-def timetable() -> None:
-    st.title("Timetable")
-    table = {"departure station": [], "departure date": [], "departure time": [], "arrival station": []}
-    for row in st.session_state["client"].query_engine.select.run.all():
-        table["departure date"].append(row.departure_date.date())
-        table["departure station"].append(row.departure_station)
-        table["departure time"].append(row.departure_time.time().strftime("%H:%M"))
-        table["arrival station"].append(row.arrival_station)
-    st.dataframe(pd.DataFrame(table))
+from src import CassandraConnector, Config
+from src.pages import *
 
 
 def main() -> None:
-    # backend
-    config = Config("config.yaml")
-    client = CassandraConnector(config)
+    client = None
+    try:
+        config = Config("config.yaml")
+        client = CassandraConnector(config)
+    except:
+        st.error("Application cannot connect to the database. Try again later.")
 
-    if 'client' not in st.session_state:
-        st.session_state["client"] = client
-
-    # ui
-    st.set_page_config(
-        page_title="BIG TRAINS",
-        page_icon="🚆",
-        layout="wide",
-        initial_sidebar_state="expanded",
-    )
-    st.sidebar.title(f"🚂 BIG TRAINS")
-    pages = {
-        "Admin panel": admin,
-        "Timetable": timetable,
-        "Reservations": reservations
-    }
-    name = st.sidebar.radio('Select step', pages.keys(), index=0)
-    pages[name]()
+    if client is not None:
+        st.set_page_config(
+            page_title="BIG TRAINS",
+            page_icon="🚆",
+            layout="wide",
+            initial_sidebar_state="expanded",
+        )
+        if 'config' not in st.session_state:
+            st.session_state["config"] = config
+        if 'client' not in st.session_state:
+            st.session_state["client"] = client
+        st.sidebar.title(f"🚂 BIG TRAINS")
+        pages = {
+            "Admin": admin_page,
+            "Client": client_page,
+            "Stress tests": stress_tests
+        }
+        name = st.sidebar.radio('Select step', pages.keys(), index=0)
+        pages[name]()
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -8,4 +8,7 @@ def fill_database(client: CassandraConnector, runs: int = 20) -> None:
         station1, station2 = mock.station, mock.station
         while station1 == station2:
             station2 = mock.station
-        client.query_engine.insert.run(station1, mock.date, mock.time, station2, mock.travel_time, mock.carrier)
+        run_id = client.query_engine.insert.run(station1, mock.date, mock.time, station2, mock.travel_time,
+                                                mock.carrier)
+        for seat in mock.carriage:
+            client.query_engine.insert.seat(run_id, seat.no, seat.type)
